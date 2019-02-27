b6013a63ef12d819186d8c1e2eeeb3e191396346
Hipoppoltark <48064845+Hipoppoltark@users.noreply.github.com>
2019-02-27 21:20:43 +0400

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,29 @@
+﻿var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/1index.html');
+});
+
+server.listen(3000, function(){
+	console.log("Starting server");
+});
+
+io.on('connection', function(socket){
+	console.log('New user!');
+	socket.on('disconnect', function(){
+		console.log("User disconnect!");
+	});
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
+
